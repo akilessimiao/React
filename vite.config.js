291157a26e19bb0d5bf4a72876959793aa6f93b9
Akiles Simião <ldtnettelecom@gmail.com>
2026-01-28 00:59:01 -0300

    Projeto PDV pronto para deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/React/',  // importante se estiver usando GitHub Pages
+  base: '/React/',   // ← obrigatório (nome do repositório)
 })

8efa58d8ad2663f86899284f768dd239b5194ee4
Akiles Simião <ldtnettelecom@gmail.com>
2026-01-29 12:54:43 -0300

    fix: corrige configuração PostCSS e Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
